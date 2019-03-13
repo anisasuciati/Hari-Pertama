f7cbc1af9bff9366b3c277048b6ff147e504591a
anisa <anisasuciati220@gmail.com>
2019-03-13 13:41:25 +0700

    sama

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>hello nama saya anisa!</h2>
+        <Gambar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Gambar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Gambar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>saya hobi menulis.</h2>
+      </div>
+    );
+  }
+}
